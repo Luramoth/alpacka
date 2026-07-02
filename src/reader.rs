ea1d1e9aa4b99c78fef0041abe45075fc0bb99a4
LuraMoth <fasreaperearnest@gmail.com>
2026-07-02 19:46:56 -0400

    change unwraps to be more useful errors for the user to deal with

--- src/reader.rs
+++ src/reader.rs
@@ -10,23 +10,26 @@ pub struct Reader{
 }
 
 impl Reader {
-    pub fn new(path: String) -> Result<Self, String> {
-        let file = File::open(path).unwrap();
+    pub fn new(path: &String) -> Result<Self, String> {
+        let file = File::open(path).map_err(|e| format!("could not open {}: {}", path, e))?;
+
+
         let mut reader = BufReader::new(file);
 
 
         let mut header_buf = [0u8; HEADER_SIZE];
 
-        reader.read_exact(&mut header_buf).expect("Couldn't read file contents");
+        if reader.read_exact(&mut header_buf).is_err() {
+            Err("header corrupt")?}
 
         let header = Header {
-            magic: u32::from_le_bytes(header_buf[0..4].try_into().unwrap()),
-            version: u32::from_le_bytes(header_buf[4..8].try_into().unwrap()),
-            entry_count: u32::from_le_bytes(header_buf[8..12].try_into().unwrap()),
-            data_offset: u32::from_le_bytes(header_buf[12..16].try_into().unwrap()),
-            string_table_offset: u32::from_le_bytes(header_buf[16..20].try_into().unwrap()),
-            index_offset: u32::from_le_bytes(header_buf[20..24].try_into().unwrap()),
-            reserved: u32::from_le_bytes(header_buf[24.. 28].try_into().unwrap()),
+            magic: u32::from_le_bytes(header_buf[0..4].try_into().map_err(|_| "header malformed")?),
+            version: u32::from_le_bytes(header_buf[4..8].try_into().map_err(|_| "header malformed")?),
+            entry_count: u32::from_le_bytes(header_buf[8..12].try_into().map_err(|_| "header malformed")?),
+            data_offset: u32::from_le_bytes(header_buf[12..16].try_into().map_err(|_| "header malformed")?),
+            string_table_offset: u32::from_le_bytes(header_buf[16..20].try_into().map_err(|_| "header malformed")?),
+            index_offset: u32::from_le_bytes(header_buf[20..24].try_into().map_err(|_| "header malformed")?),
+            reserved: u32::from_le_bytes(header_buf[24.. 28].try_into().map_err(|_| "header malformed")?),
         };
 
         if header.magic != MAGIC_NUMBER{
@@ -34,31 +37,38 @@ impl Reader {
         if header.version > VERSION{
             Err("Version of Alpack archive is beyond current capabilities")?}
 
-        reader.seek(SeekFrom::Start(header.index_offset as u64)).expect("Invalid archive index offset");
+        if reader.seek(SeekFrom::Start(header.index_offset as u64)).is_err() {
+            Err("Broken Index offset")?}
+
         let mut entries: HashMap<String, Entry> = HashMap::with_capacity(header.entry_count as usize);
 
         for _ in 0..header.entry_count {
             let mut entry_buf = [0u8; ENTRY_SIZE];
 
-            reader.read_exact(&mut entry_buf).expect("Could not read Entry");
+            if reader.read_exact(&mut entry_buf).is_err() {
+                println!("Error: Invalid entry, skipping");
+                continue;
+            }
 
             let entry = Entry {
-                custom1: u32::from_le_bytes(entry_buf[0..4].try_into().unwrap()),
-                custom2: u32::from_le_bytes(entry_buf[4..8].try_into().unwrap()),
-                data_offset: u32::from_le_bytes(entry_buf[8..12].try_into().unwrap()),
-                compressed_size: u32::from_le_bytes(entry_buf[12..16].try_into().unwrap()),
-                original_size: u32::from_le_bytes(entry_buf[16..20].try_into().unwrap()),
-                compression_type: u32::from_le_bytes(entry_buf[20..24].try_into().unwrap()),
-                name_offset: u32::from_le_bytes(entry_buf[24..28].try_into().unwrap()),
-                reserved: u32::from_le_bytes(entry_buf[28..32].try_into().unwrap()),
+                custom1: u32::from_le_bytes(entry_buf[0..4].try_into().map_err(|_| "entry malformed")?),
+                custom2: u32::from_le_bytes(entry_buf[4..8].try_into().map_err(|_| "entry malformed")?),
+                data_offset: u32::from_le_bytes(entry_buf[8..12].try_into().map_err(|_| "entry malformed")?),
+                compressed_size: u32::from_le_bytes(entry_buf[12..16].try_into().map_err(|_| "entry malformed")?),
+                original_size: u32::from_le_bytes(entry_buf[16..20].try_into().map_err(|_| "entry malformed")?),
+                compression_type: u32::from_le_bytes(entry_buf[20..24].try_into().map_err(|_| "entry malformed")?),
+                name_offset: u32::from_le_bytes(entry_buf[24..28].try_into().map_err(|_| "entry malformed")?),
+                reserved: u32::from_le_bytes(entry_buf[28..32].try_into().map_err(|_| "entry malformed")?),
             };
 
-            reader.seek(SeekFrom::Start((header.string_table_offset + entry.name_offset) as u64)).expect("Invalid String tame offset");
-            let mut name = String::new();
-            let err = reader.read_to_string(&mut name);
+            if reader.seek(SeekFrom::Start((header.string_table_offset + entry.name_offset) as u64)).is_err() {
+                println!("Error: invalid String Table offset, skipping entry.");
+                continue;
+            }
 
-            if err.is_err() {
-                println!("Invalid string table entry. name offset: {0}", entry.name_offset);
+            let mut name = String::new();
+            if reader.read_to_string(&mut name).is_err() {
+                println!("Error: invalid String table entry, skipping.");
                 continue;
             }
 
