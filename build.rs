5a82a28fb7a145e4673eac396d262b0958e3684c
LuraMoth <fasreaperearnest@gmail.com>
2026-07-05 02:53:59 -0400

    create warning for people building on 32 bit

---
+++ build.rs
@@ -0,0 +1,13 @@
+﻿fn main() {
+    let pointer_width = std::env::var("CARGO_CFG_TARGET_POINTER_WIDTH")
+        .expect("CARGO_CFG_TARGET_POINTER_WIDTH should always be set by Cargo");
+
+    if pointer_width != "64" {
+        println!(
+            "cargo:warning=Alpacka assumes a 64-bit `usize` (sizes/offsets are stored as u64 on disk).\
+            Building for a 32-bit target means any archive entry or file above ~4.29GB will silently \
+            truncate instead of erroring. this is unsupported, but I will not stop you from trying \
+            - proceed at your own risk"
+        );
+    }
+}
